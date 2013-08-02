133a031b816d3bf7a394d909f50d0dbf60e4d0d6
Kevin Mees <kev.mees@gmail.com>
2013-08-02 15:23:24 +0200

    Use the sinonjs bower package instead of npm module. The sinon npm module doesnt work in the browser and therefore not with karma.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var framework = function(files) {
   files.unshift(pattern(path.join(__dirname, 'adapter.js')));
   files.unshift(pattern(path.resolve(require.resolve('sinon-chai'))));
   files.unshift(pattern(path.resolve(require.resolve('chai'), '../chai.js')));
-  files.unshift(pattern(path.resolve(require.resolve('sinon'))));
+  // TODO is there a bower api for resolving paths?
+  files.unshift(pattern(path.join(__dirname, 'bower_components/sinonjs/sinon.js')));
 };
 
 framework.$inject = ['config.files'];
