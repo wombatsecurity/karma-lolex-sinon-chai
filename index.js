5719376fdd77514212b3e419db009ccc38c39588
Kevin Mees <kev.mees@gmail.com>
2013-08-02 11:37:37 +0200

    Added sinon and sinon-chai support and renamed framework to 'sinon-chai'

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var pattern = function(file) {
 
 var framework = function(files) {
   files.unshift(pattern(path.join(__dirname, 'adapter.js')));
+  files.unshift(pattern(path.resolve(require.resolve('sinon-chai'))));
   files.unshift(pattern(path.resolve(require.resolve('chai'), '../chai.js')));
+  files.unshift(pattern(path.resolve(require.resolve('sinon'))));
 };
 
 framework.$inject = ['config.files'];
-module.exports = {'framework:chai': ['factory', framework]};
+module.exports = {'framework:sinon-chai': ['factory', framework]};
