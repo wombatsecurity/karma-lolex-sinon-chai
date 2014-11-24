c3a54cfc3a3bfa7571f99b80af147849f0d5ea2a
Jonathan Kim <me@jonathan-kim.com>
2014-11-23 22:24:34 -0500

    Remove crazy dependency stuff.

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var pattern = function(file) {
 var framework = function(files) {
   files.unshift(pattern(path.join(__dirname, 'adapter.js')));
   files.unshift(pattern(path.resolve(require.resolve('sinon-chai'))));
-  files.unshift(pattern(path.resolve(require.resolve('chai'), '../chai.js')));
-  // TODO is there a bower api for resolving paths?
-  files.unshift(pattern(path.join(__dirname, 'bower_components/sinonjs/sinon.js')));
 };
 
 framework.$inject = ['config.files'];
