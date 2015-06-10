01785e860da104e66e4c2ef8e8b3873ab760f363
Kevin Mees <kev.mees@gmail.com>
2015-06-10 13:02:01 +0200

    Update chai to ~3.0, sinon to ~1.15 and sinon-chai to ~2.8

--- index.js
+++ index.js
@@ -12,9 +12,37 @@ var endsWith = function(substr) {
 };
 
 var framework = function(files) {
-  var sinonPath = path.resolve(require.resolve('sinon'), '../../pkg/sinon.js');
+  files.unshift(pattern(path.resolve(require.resolve('sinon-chai'))));
+
+  var sinonPath = path.resolve(require.resolve('sinon'), '../sinon.js');
   if (!_(files).map('pattern').find(endsWith(path.relative(__dirname, sinonPath)))) {
-    files.unshift(pattern(sinonPath));
+    var sinonBasePath = path.dirname(sinonPath);
+    var sinonFiles = [
+      "./sinon.js",
+      "./sinon/util/core.js",
+      "./sinon/extend.js",
+      "./sinon/typeOf.js",
+      "./sinon/times_in_words.js",
+      "./sinon/spy.js",
+      "./sinon/call.js",
+      "./sinon/behavior.js",
+      "./sinon/stub.js",
+      "./sinon/mock.js",
+      "./sinon/collection.js",
+      "./sinon/assert.js",
+      "./sinon/sandbox.js",
+      "./sinon/test.js",
+      "./sinon/test_case.js",
+      "./sinon/match.js",
+      "./sinon/format.js",
+      "./sinon/log_error.js"
+    ];
+
+    _.forEach(sinonFiles.reverse(), function(relativeFile) {
+      var absoluteFile = path.join(sinonBasePath, relativeFile);
+
+      files.unshift(pattern(absoluteFile));
+    });
   }
 
   var chaiPath = path.resolve(require.resolve('chai'), '../chai.js');
@@ -22,8 +50,6 @@ var framework = function(files) {
     files.unshift(pattern(chaiPath));
     files.push(pattern(path.join(__dirname, 'chai-adapter.js')));
   }
-
-  files.push(pattern(path.resolve(require.resolve('sinon-chai'))));
 };
 
 framework.$inject = ['config.files'];
